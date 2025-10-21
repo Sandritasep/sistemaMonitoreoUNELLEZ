2369e0c9d16fecd17e839af5c08df7ec923c96cc
Sandra Sepulveda <sandrasepulvedam18@gmail.com>
2025-10-21 16:59:27 -0400

    comit porsia

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btnOpen_iniciar = document.querySelector(".abrir-modal");
 const open_modal = document.querySelector(".modal-completo");
 const btn_cerrarM = document.querySelector(".modal-close");
+const cedulaIn = document.getElementById("cedula");
+const tipoCedula = document.getElementById("Nacionalidad");
 
 
 btnOpen_iniciar.addEventListener("click", function(){
@@ -8,5 +10,12 @@ btnOpen_iniciar.addEventListener("click", function(){
 });
 
 btn_cerrarM.addEventListener("click", function(){
+    limpiarForm();
     open_modal.classList.remove("active");
 });
+
+function limpiarForm(){
+    cedulaIn.value = "";
+    tipoCedula.value = "V";
+
+}
